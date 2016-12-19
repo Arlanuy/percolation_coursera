79f7b15ec4d418c9b7e918882d01cff7a9c6161b
Arlan Uy <gnhs.com@gmail.com>
2016-12-19 22:45:25 +0900

    Inputs of rows and columns working

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,18 +2,54 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 
 public class Percolation {
 	boolean[][] n_square;
+	static int[] rowsandcolumns_array;
+	int n;	
 	WeightedQuickUnionUF wquf; 
 	
 	public Percolation(int n) {
+		this.n = n;
 		n_square = new boolean[n][n];
 		wquf = new WeightedQuickUnionUF(n);
+		int i = 0, row = 0, col = 0;
+		for (int num: rowsandcolumns_array) {
+			if (i % 2 == 0) {
+				row = num;
+			}
+			
+			else {
+				col = num;
+				StdOut.println("row: " + row + " col: " + col + " xyTo1D: " + xyTo1D(row, col));
+			}
+			if (checkValidIndex(row, col) == false) break;
+			i++;
+		}
 		
 	}  // create n-by-n grid, with all sites blocked
-	   
+	
+	private boolean checkValidIndex(int row, int col) throws ArrayIndexOutOfBoundsException{
+		if (row > n || col > n) {
+			try {
+				throw new ArrayIndexOutOfBoundsException();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				StdOut.println("The row: " + row + " or column: " + col + " exceeded n " + n);
+			}
+			return false;
+		}
+		return true;
+
+	}
+	
+	private int xyTo1D(int row, int col) {
+		return (((row - 1) * n) + (col - 1));
+		
+	}
+	
+	/**   
 	public void open(int row, int col) {
 	
 	}       // open site (row, col) if it is not open already 
@@ -30,10 +66,14 @@ public class Percolation {
 	public boolean percolates() {
 		
 	}             // does the system percolate?
-
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = StdIn.readInt() 
+		StdOut.print("What is the value for n: ");
+		int n = StdIn.readInt();
+		StdOut.print("What are the value of rows and column: ");
+		rowsandcolumns_array = StdIn.readAllInts();
+		Percolation p = new Percolation(n);
 	}
 
 }
